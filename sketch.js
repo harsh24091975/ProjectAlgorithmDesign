bd6c6553e13b4471d4dba9d7024ed5453f104b6f
harsh24091975 <74312669+harsh24091975@users.noreply.github.com>
2020-11-17 21:07:22 +0530

    created new library

--- sketch.js
+++ sketch.js
@@ -1,31 +1,33 @@
 var fixedRect, movingRect; 
 
 function setup() { 
-  createCanvas(800,400); 
-  fixedRect = createSprite(600,400,50,80); 
+  createCanvas(1200,800); 
+  fixedRect =  createSprite(400, 100, 50, 80);
   fixedRect.shapeColor = "green"; 
-  movingRect = createSprite(400,200,80,30);
+  movingRect = createSprite(400, 800,80,30);
   movingRect.shapeColor = "green"; 
+  movingRect.velocityY=-5;
+  fixedRect.velocityY=5;
 } 
 
 function draw() {
-  background(255,255,255);
-  console.log("Distance 1 : ",fixedRect.x-movingRect.x," Distance 2 : ",fixedRect.width/2+movingRect.width/2);
-  console.log("Distance 5 : ",fixedRect.y-movingRect.y," Distance 6 : ",fixedRect.width/2+movingRect.width/2);
-  if(movingRect.x-fixedRect.x < fixedRect.width/2+movingRect.width/2 &&
-    fixedRect.x-movingRect.x < fixedRect.width/2+movingRect.width/2 && 
-    movingRect.y-fixedRect.y < fixedRect.width/2+movingRect.width/2 &&
-    fixedRect.y-movingRect.y < fixedRect.width/2+movingRect.width/2 ){ 
-    fixedRect.shapeColor = "red";
-    movingRect.shapeColor = "red";
-  } 
-  else{
-    fixedRect.shapeColor = "green"; 
-    movingRect.shapeColor = "green"; 
+  background("black");
+  if (isTouching(movingRect,fixedRect) ){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
   }
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+  else{
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+ bounceOff(movingRect,fixedRect);
+ 
+ // movingRect.x = World.mouseX;
+  //movingRect.y = World.mouseY;
   drawSprites();
 
 
 }
+
+
+
